94757bfd4ce00a399e02f47f1759cb519c98fe2f
Inez Korczyński <korczynski@gmail.com>
2021-07-23 17:41:46 -0700

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,4 +1,8 @@
 echo "Hello world!"
-/android/sdk/launch-emulator.sh &
+/android/sdk/launch-emulator.sh > launch-emulator.log 2>&1 &
 sleep 60
 /android/sdk/platform-tools/adb shell getprop dev.bootcomplete
+pwd
+ls -al
+/android/sdk/platform-tools/adb install app.apk
+/android/sdk/platform-tools/adb install androidTest.apk
