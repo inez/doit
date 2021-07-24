a4f3f9401cadc12f1fe21e2a685590bccecd8075
Inez Korczyński <korczynski@gmail.com>
2021-07-23 18:21:54 -0700

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,7 +1,11 @@
 echo "Hello world!"
 /android/sdk/launch-emulator.sh > launch-emulator.log 2>&1 &
-sleep 60
+sleep 75
+echo "Hello world 1!"
 /android/sdk/platform-tools/adb shell getprop dev.bootcomplete
+/android/sdk/platform-tools/adb shell getprop dev.bootcomplete
+/android/sdk/platform-tools/adb shell getprop dev.bootcomplete
+echo "Hello world 2!"
 pwd
 ls -al
 /android/sdk/platform-tools/adb install /app.apk
