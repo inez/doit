51abf88558b3d2fa62457ba45a37d9cfb63c3efe
Inez Korczyński <korczynski@gmail.com>
2021-07-23 18:37:30 -0700

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,6 +1,6 @@
 echo "Hello world!"
 /android/sdk/launch-emulator.sh > launch-emulator.log 2>&1 &
-sleep 45
+sleep 90
 /android/sdk/platform-tools/adb install /app.apk
 /android/sdk/platform-tools/adb install /androidTest.apk
 /android/sdk/platform-tools/adb shell am instrument -w -m --no-window-animation -e debug false -e class com.squareup.instrumentation.tests.ActivateSquareCardSwipeTest#swipeForCardVerification com.squareup.instrumentation
