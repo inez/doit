d1b5ab3098d48747d37d001e764b5fa479356ccf
Inez Korczyński <korczynski@gmail.com>
2021-07-24 20:36:36 -0700

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,8 +1,9 @@
-echo "Hello world 24!"
+#!/bin/bash
+echo "Hello world 24.1!"
+/android/sdk/launch-emulator.sh
 # sleep $[ ( $RANDOM % 30 )  + 1 ]s
 # /android/sdk/launch-emulator.sh > launch-emulator.log 2>&1 &
-/android/sdk/launch-emulator.sh
-#sleep 120
+# sleep 120
 # /android/sdk/platform-tools/adb install /app.apk
 # /android/sdk/platform-tools/adb install /androidTest.apk
 # /android/sdk/platform-tools/adb shell am instrument -w -m --no-window-animation -e debug false -e class com.squareup.instrumentation.tests.ActivateSquareCardSwipeTest#swipeForCardVerification com.squareup.instrumentation
