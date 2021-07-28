415fe4dbadd65d2c233c81c65aecc8201b347a94
Inez Korczyński <korczynski@gmail.com>
2021-07-28 12:23:52 -0700

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "Hello world 25.1/1!"
-/android/sdk/launch-emulator.sh > launch-emulator.log 2>&1 &
-echo "Hello world 25.2!"
-while [ "`/android/sdk/platform-tools/adb shell getprop sys.boot_completed | tr -d '\r' `" != "1" ] ; do sleep 1; done
-echo "Hello world 25.3!"
-/android/sdk/platform-tools/adb shell getprop dev.bootcomplete ; /android/sdk/platform-tools/adb install /app.apk ; /android/sdk/platform-tools/adb install /androidTest.apk ; /android/sdk/platform-tools/adb shell am instrument -w -m --no-window-animation -e debug false -e class com.squareup.instrumentation.tests.ActivateSquareCardSwipeTest#swipeForCardVerification com.squareup.instrumentation
+
+echo "Hello world!"
+cat /not/
